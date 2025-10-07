9661fff2468845cde2e74e63dc49d6119c7a098d
Abhisek-GenEvo <chakrabortyabhi2013@gmail.com>
2025-10-07 16:45:47 +0200

    Create PECAT_assembly.sh

---
+++ Genome_assembly/PECAT_assembly.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J pecat            # Job name
+#SBATCH -o pecat.%j.out       # Specify stdout output file (%j expands to jobId)
+#SBATCH -p bigmem                   # Queue name 'smp' or 'parallel' on Mogon II
+#SBATCH -n 1                     # Total number of tasks, here explicitly 1
+#SBATCH -c 20                    # Total number of cores for the single task
+#SBATCH --mem 350G                # The default is 300M memory per job. You'll likely have to adapt this to your needs
+#SBATCH -t 24:00:00              # Run time (hh:mm:ss)
+
+#SBATCH -A m2_jgu-evoltroph     # Specify allocation to charge against
+
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=achakrab@uni-mainz.de
+
+## Load all necessary modules if needed
+module load tools/Singularity/3.9.1
+source /lustre/project/m2_jgu-evoltroph/achakrab/anaconda3/bin/activate
+conda activate Assembly_env
+pecat.pl unzip pecat_cfgfile.cfg
